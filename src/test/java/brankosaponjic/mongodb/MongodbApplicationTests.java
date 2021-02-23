626ef224364ec2af925f9c038985e21346134bd7
Branko <brankosaponjic76@gmail.com>
2021-02-23 17:48:42 +0100

    Tested the findAll and delete methods. Closes #5

--- src/test/java/brankosaponjic/mongodb/MongodbApplicationTests.java
+++ src/test/java/brankosaponjic/mongodb/MongodbApplicationTests.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class MongodbApplicationTests {
 
@@ -26,6 +31,19 @@ class MongodbApplicationTests {
         product2.setPrice(1200f);
         Product savedProduct2 = productRepository.save(product2);
         Assert.notNull(savedProduct2,"Mustn't be null!");
+    }
+
+    @Test
+    void testFindAll() {
+        List<Product> products = productRepository.findAll();
+        assertEquals(3, products.size());
+    }
+
+    @Test
+    void testDelete() {
+        productRepository.deleteById("60352bb79599fb557bedd240");
+        Optional<Product> product = productRepository.findById("60352bb79599fb557bedd240");
+        assertEquals(Optional.empty(), product);
     }
 
 }
