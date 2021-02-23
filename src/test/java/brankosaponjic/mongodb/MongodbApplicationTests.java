2cb8fb368abea7c133194069d0c243731098da6a
Branko <brankosaponjic76@gmail.com>
2021-02-23 16:26:34 +0100

    Created Test Save. Closes #3

--- src/test/java/brankosaponjic/mongodb/MongodbApplicationTests.java
+++ src/test/java/brankosaponjic/mongodb/MongodbApplicationTests.java
@@ -1,13 +1,31 @@
 package brankosaponjic.mongodb;
 
+import brankosaponjic.mongodb.entities.Product;
+import brankosaponjic.mongodb.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class MongodbApplicationTests {
 
+    @Autowired
+    ProductRepository productRepository;
+
     @Test
-    void contextLoads() {
+    void testSave() {
+        Product product = new Product();
+        product.setName("Mac Book Pro");
+        product.setPrice(2000f);
+        Product savedProduct = productRepository.save(product);
+        Assert.notNull(savedProduct,"Mustn't be null!");
+
+        Product product2 = new Product();
+        product2.setName("Iphone 12 Pro");
+        product2.setPrice(1200f);
+        Product savedProduct2 = productRepository.save(product);
+        Assert.notNull(savedProduct2,"Mustn't be null!");
     }
 
 }
